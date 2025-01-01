eaa41f32907512dc7657f1a85ab79cbf7b157a5a
Hak Min Kim <kimhakmin@google.com>
2025-01-01 09:56:54 +0900

    initial

--- providers.tf
+++ providers.tf
@@ -16,17 +16,6 @@ provider "google-beta" {
   region                = var.region # 리전 변수 추가
 }
 
-# Kubernetes Provider 설정 (기존 클러스터 연결)
-provider "kubernetes" {
-  # config_path = "~/.kube/config"  # 로컬 kubeconfig 파일 사용 시
-  # 또는 host, client_certificate, client_key, cluster_ca_certificate 등을 사용하여 직접 설정 가능
-
-  # 예시: Google Kubernetes Engine (GKE) 클러스터에 연결
-  host                   = data.google_container_cluster.my_cluster.endpoint
-  token                  = data.google_client_config.current.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)
-}
-
 # Helm Provider 설정
 provider "helm" {
   kubernetes {
